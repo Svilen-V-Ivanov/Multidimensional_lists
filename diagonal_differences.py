f29c3a2c36dbdf6a2b0a8ba4eec1e4e384a0538b
Svilen Ivanov <102068545+TheShadowTM@users.noreply.github.com>
2022-05-26 18:52:06 +0300

    Exercises Second Problem

---
+++ diagonal_differences.py
@@ -0,0 +1,16 @@
+size = int(input())
+
+matrix = []
+
+for _ in range(size):
+    matrix.append([int(x) for x in input().split(' ')])
+primary = []
+secondary = []
+
+for prim in range(size):
+    primary.append(matrix[prim][prim])
+    secondary.append(matrix[prim][size - 1 - prim])
+
+difference = abs(sum(primary) - sum(secondary))
+
+print(difference)
